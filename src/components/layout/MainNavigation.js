14142bf1efe7f89c6efccf16b3f24ac553202a4a
Your Name <tonfroy90@gmail.com>
2023-03-22 17:18:31 -0700

    Completed MVP v1 3/22/23

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,11 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import styleClasses from "./MainNavigation.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
   return (
     <header className={styleClasses.header}>
       <div className={styleClasses.logo}>Meetups</div>
@@ -15,7 +18,12 @@ function MainNavigation() {
             <Link to="/new-meetup">New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites">Favorites</Link>
+            <Link to="/favorites">
+              My Favorites
+              <span className={styleClasses.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,13 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
-
 function NewMeetupPage() {
+  const history = useNavigate();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://meetups-react-f4c7f-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history("/");
+    });
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
-  )
+  );
 }
 
 export default NewMeetupPage;
